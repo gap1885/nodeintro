8cc0552eb2196c17c184f6eae9a717ea4bdf348c
Gabriela Arias <gap1885@gmail.com>
2018-09-11 12:28:53 +0200

    a

---
+++ main.js
@@ -0,0 +1,14 @@
+"use sctrict";
+const functions = require('./functions')
+const chalk = require('chalk');
+let counter = 0;
+const intervalId = setInterval(() => {
+    const option = functions.getRandomOption();
+    console.log(chalk.red(option));
+    counter++;
+    if (counter=== 10) {
+        clearInterval(intervalId);
+    }
+}, 5000);
+
+console.log(chalk.yellow(functions.options));
